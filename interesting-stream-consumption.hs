a57eefb07c621f5f159e4a3573bfe1e5d63ca575
Ketzacoatl <ketzacoatl@users.noreply.github.com>
2018-12-04 00:00:36 -0500

    add "interesting stream consumption" example

---
+++ interesting-stream-consumption.hs
@@ -0,0 +1,15 @@
+#!/usr/bin/env stack
+-- stack --resolver lts-12.20 --install-ghc runghc --package classy-prelude-conduit
+
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+import ClassyPrelude.Conduit
+
+main :: IO ()
+main = do
+  total <- runConduit $ yieldMany [1..10] .| mapC (* 10) .| sumC
+  print total
+
+  -- or more explicitly...
+  explicitTotal <- runConduit $ yieldMany [1..10] .| mapC (* 10) .| foldlC (+) 0
+  print explicitTotal
