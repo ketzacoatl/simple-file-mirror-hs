187c87f8210f27b0280707349b47975de6622c35
Ketzacoatl <ketzacoatl@users.noreply.github.com>
2018-12-03 23:57:29 -0500

    add await and yield example to multiply list by 10

---
+++ await-and-yield.hs
@@ -0,0 +1,9 @@
+#!/usr/bin/env stack
+-- stack --resolver lts-12.20 --install-ghc runghc --package classy-prelude-conduit
+
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+import ClassyPrelude.Conduit
+
+main :: IO ()
+main = runConduit $ yieldMany [1..10] .| mapC (* 10) .| mapM_C print
